f4f2ab270cbbede3ac3fe760a2d5f79e41631654
David O'Gwynn <david_ogwynn@nextstepinnovation.com>
2021-12-14 14:08:03 -0600

    wip

--- nsi/__init__.py
+++ nsi/__init__.py
@@ -2,6 +2,6 @@ from . import (
     toolz, logging,
     
     ssh, parallel, yaml, rest, shell, markdown, graph, templates, cli, signature, 
-    bloodhound, data, config, ntlm,
+    bloodhound, data, config, ntlm, secretsdump,
 )
 __version__ = '0.0.1'
