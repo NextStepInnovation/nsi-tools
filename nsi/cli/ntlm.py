b38cf573ce6f4f6eed73a3e7f67c4a22333297ea
David O'Gwynn <david_ogwynn@nextstepinnovation.com>
2022-04-07 10:58:19 -0500

    refactor of NTLM/hash functions/scripts

--- nsi/cli/ntlm.py
+++ nsi/cli/ntlm.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import itertools
 import pprint
+import re
 from re import search
 
 import click
@@ -12,6 +13,86 @@ from .. import ntlm, logging, parallel
 from . import common
 
 log = new_log(__name__)
+
+@click.command()
+@click.option(
+    '-h', '--hashes',
+    help=('Path of file to parse for hashes. If empty, pull from stdin.'),
+    type=click.Path(exists=True), required=True,
+)
+@click.option(
+    '-p', '--potfile',
+    help=('Path of file to parse for hashes. If empty, pull from stdin.'),
+    type=click.Path(exists=True), required=True,
+)
+@click.option(
+    '-a', '--admins', type=click.Path(exists=True),
+    help=('Path of file with domain/local administrator names'),
+)
+@click.option(
+    '-T', '--table', is_flag=True,
+    help=('Output in Markdown table form'),
+)
+@click.option(
+    '-C', '--to-clipboard', is_flag=True,
+    help=('Send output to clipboard'),
+)
+@click.option(
+    '--loglevel', default='info',
+)
+def resolve(hashes, potfile, admins, table, to_clipboard, loglevel):
+    logging.setup_logging(loglevel)
+
+    ntlm_re = re.compile(r'^[0-9a-f]{32}:.*')
+    u2h = pipe(
+        hashes,
+        slurp,
+        ntlm.get_sam_hashes,
+        groupby('full_user'),
+        valmap(first),
+        valmap(get('hashes')),
+    )
+    h2pt = pipe(
+        Path(potfile).read_text().splitlines(),
+        map(lambda l: l.strip()),
+        filter(ntlm_re.search),
+        map(lambda l: l.split(':')),
+        dict
+    )
+
+    admins = pipe(
+        admins,
+        slurplines,
+        map(strip),
+        map(lower),
+        set,
+    ) if admins else set()
+    if admins:
+        log.info(
+            f'Found {len(admins)} admin account names'
+        )
+
+    def is_admin(u: str):
+        name = u.split('/')[-1]
+        return name in admins
+
+    def tr_f(u, h):
+        return f'| `{u or " "}` | `{h2pt[h] or " "}` |' + (
+            (f' **Yes** |' if is_admin(u) else '  |') 
+            if admins else ''
+        )
+
+    row_formatter = tr_f if table else lambda u, h: f'{u}\t{h2pt[h]}'
+
+    return pipe(
+        u2h.items(),
+        sorted,
+        vmap(lambda u, h: (u, h.split(':')[-1])),
+        vfilter(lambda u, h: h in h2pt),
+        vmap(row_formatter),
+        '\n'.join,
+        pyperclip.copy if to_clipboard else print,
+    )
 
 @click.command()
 @click.argument(
