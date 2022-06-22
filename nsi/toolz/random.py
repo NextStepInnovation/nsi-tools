a048ebcd8700bdc8f41a08fa2af4d4c0b9a8ecf6
David O'Gwynn <david_ogwynn@nextstepinnovation.com>
2022-06-22 11:13:57 -0500

    wip

--- nsi/toolz/random.py
+++ nsi/toolz/random.py
@@ -10,13 +10,27 @@ from .common import pipe, curry
 #
 # ----------------------------------------------------------------------
 
-def random_str(n=8, *, rng=None, exclude: Sequence = None):
+@curry
+def random_str(n=8, *, rng=None, population: Sequence = string.ascii_letters, 
+               exclude_chars: Sequence[str] = (),
+               exclude_words: Sequence[str] = (), try_index=0, max_tries=100):
+    if try_index + 1 == max_tries:
+        raise RecursionError(
+            f'Could not find a suitable random word with population: {population}'
+            f' and exclude_words: {exclude_words}'
+        )
     rng = rng or random
-    r_str = ''.join(rng.choice(string.ascii_letters) for _ in range(n))
-    if exclude and r_str in exclude:
-        return random_str(n, rng=rng, exclude=exclude)
+    clean_pop = pipe(set(population) - set(exclude_chars), tuple)
+    r_str = ''.join(rng.choice(population) for _ in range(n))
+    if exclude_words and (r_str in exclude_words):
+        return random_str(
+            n, rng=rng, population=population,
+            exclude_chars=exclude_chars, exclude_words=exclude_words,
+            try_index = try_index + 1, max_tries=max_tries,
+        )
     return r_str
 
+@curry
 def random_sentence(w=10, *, rng=None):
     rng = rng or random
     return pipe(
@@ -26,10 +40,12 @@ def random_sentence(w=10, *, rng=None):
         lambda s: s + '.'
     )
 
+@curry
 def random_user(n=8, *, rng=None):
     rng = rng or random
     return ''.join(rng.choice(string.ascii_lowercase) for _ in range(n))
 
+@curry
 def random_pw(n=16, *, rng=None, pop=string.printable[:64]):
     rng = rng or random
     return ''.join(rng.choice(pop) for _ in range(n))
