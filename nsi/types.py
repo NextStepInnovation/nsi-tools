6e04c6657e6e2f23001dfa4ed74826fea2a372ff
David O'Gwynn <david_ogwynn@nextstepinnovation.com>
2023-08-23 16:16:57 -0500

    wip

---
+++ nsi/types.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import typing as T
+
+from . import logging
+
+log = logging.new_log(__name__)
+
+Url = T.NewType('Url', str)
+Ip = T.NewType('Ip', str)
+IpList = T.NewType('IpList', T.Sequence[Ip])
+Regex = T.NewType('Regex', str)
+RegexList = T.NewType('RegexList', T.Sequence[Regex])
+Mac = T.NewType('Mac', str)
+Port = T.NewType('Port', int)
+Protocol = T.NewType('Protocol', str)
+Timestamp = T.NewType('Timestamp', str)
+Int = T.NewType("Int", str)
+Float = T.NewType('Float', str)
+Html = T.NewType('Html', str)
+ErrorJson = T.NewType('ErrorJson', dict)
