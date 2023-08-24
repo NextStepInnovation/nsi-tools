7777710918b5c637dee2f6e1419f1e65c0b66225
David O'Gwynn <david_ogwynn@nextstepinnovation.com>
2023-08-24 14:44:17 -0500

    wip

---
+++ nsi/nexpose/scan_templates.py
@@ -0,0 +1,37 @@
+import typing as T
+import functools
+import pprint
+
+from requests import Response
+
+from .. import logging
+from ..toolz import *
+from .. import yaml
+from ..rest import Api, get_json
+from .api import (
+    get_iterator, get_iterator500, 
+    NexposeApiError, method_body, handle_error_response,
+)
+from .types import (
+    AssetList, 
+    Site, SiteMap, SiteId, SiteNexposeId, 
+    ScanEngine, ScanEngineId,
+    ScanEngineMap,
+)
+
+log = logging.new_log(__name__)
+
+get_engines = get_iterator(['scan_engines'])
+
+@functools.cache
+def template_map(api: Api) -> ScanEngineMap:
+    log.info('Loading Nexpose scan engine map...')
+    engines = tuple(get_engines(api)())
+    log.info(f'  .. {len(engines)} engines loaded.')
+    return pipe(
+        merge(
+            {r['name']: r for r in engines},
+            {r['id']: r for r in engines},
+        ), 
+    )
+
